0aa6b0ed5c2a21bbda0d849a3054ba62b28d5434
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-11-08 01:43:41 +0200

    ..

--- Direct_Methods/forwardSub.py
+++ Direct_Methods/forwardSub.py
@@ -1,18 +1,25 @@
 import copy
-
+import numpy as np
 from Direct_Methods.commonFunctions import *
 
-# ragy
 def forwardSubstitution(matrix, b):
-   matrix_copy = copy.deepcopy(matrix)
-   answer = []
-   for i in range(0, len(matrix)):
+   # backward elimination is necessary
+   # b = np.array(b, dtype=float)
+   answer = np.array(dtype=float)
+   rows = len(matrix)
+   
+   for i in range(rows):
+      ans = b[i]
+      
       if matrix[i][i] == 0:
          if b[i] != 0:
             return None
          else:
-            answer.append(f"x{subscript(i + 1)}") # no need for this
-      else:
-         answer.append(roundBy(b[i] / matrix[i][i]))
-   
+            answer.append(f"x{subscript(i + 1)}")
+      else: 
+         for j in range(0, i):
+            ans -= matrix[i][j] * answer[j]
+         
+         answer[i] = ans / matrix[i][i]
+
    return answer
