b86169cd8cc5d66a48a7fec96a1a1bd470ae20ab
Nour-Eldien <noureldientaha@gmail.com>
2024-11-10 00:04:19 +0200

    added forward elimination function and commonFuntions

--- Direct_Methods/Direct.py
+++ Direct_Methods/Direct.py
@@ -1,9 +1,57 @@
+from commonFunctions import *
 
 import numpy as np
 
+def forwardElimination(matrixA, vectorB):
+   multipliers = []
+   rows_order = np.arange(len(matrixA))
+   rows = matrixA.shape[0]
 
-def forwardElimination(arr):
-    pass
+   for r in range(rows - 1):
+      
+      pivot_forward(r, matrixA, rows_order, vectorB)
+
+      for i in range(r + 1, rows):
+         
+         if matrixA[i, r] == 0.0:
+            multipliers.append(0.0)
+            continue
+
+         multiplier = matrixA[i, r] / matrixA[r, r]
+         multiplier = roundBy(multiplier)
+         multipliers.append(multiplier)
+         
+         matrixA[i, r+1:] -= matrixA[r, r+1:] * multiplier
+         matrixA[i, r+1:] = np.vectorize(roundBy)(matrixA[i, r+1:])
+
+         vectorB[i] -= vectorB[r] * multiplier
+         vectorB[i] = roundBy(vectorB[i])
+
+         matrixA[i, r] = 0.0
+   
+   multipliers = np.array(multipliers, dtype=float)
+   rows_order = np.array(rows_order, dtype=int)
+
+   return matrixA, vectorB, multipliers, rows_order
+
+
+def pivot_forward(row, matrixA, row_order, vectorB):
+    n = matrixA.shape[0]
+    max_row = row
+    
+    for i in range(row + 1, n):
+        if abs(matrixA[i, row]) > abs(matrixA[max_row, row]):
+            max_row = i
+
+    if max_row != row:
+        # Swap rows in the matrix
+        matrixA[[row, max_row], :] = matrixA[[max_row, row], :]
+        
+        # Swap elements in the vector
+        vectorB[[row, max_row]] = vectorB[[max_row, row]]
+        
+        # Swap row order
+        row_order[[row, max_row]] = row_order[[max_row, row]]
 
 
 def round_to_sig_figs(x, sig_figs):
---
+++ Direct_Methods/commonFunctions.py
@@ -0,0 +1,38 @@
+import math
+
+significant_digits = 4
+
+subscripts = {0: '₀', 1: '₁', 2: '₂', 3: '₃', 4: '₄', 5: '₅', 6: '₆', 7: '₇', 8: '₈', 9: '₉'}
+
+def printMatrix(matrix):
+   for row in matrix:
+      print(" ".join(f"{val:.2f}" for val in row))
+   print()
+
+def print_matrix_vector(u, b):
+    # Print the matrix u with proper formatting
+    print("Matrix u:")
+    for row in u:
+        print(f"[{', '.join([f'{num:.1f}' if isinstance(num, float)
+              and num == int(num) else str(num) for num in row])}]")
+
+    # Print the vector b with proper formatting
+    print("\nVector b:")
+    print(f"[{', '.join([f'{num:.1f}' if isinstance(num, float)
+          and num == int(num) else str(num) for num in b])}]")
+
+def subscript(num):
+   ret = ""
+   
+   if num == 0:
+      return subscripts[0]  # Handle zero case
+   
+   while num != 0:
+      ret += subscripts[num % 10]
+      num //= 10
+   return ''.join(reversed(ret))
+
+def roundBy(num):
+   if num == 0:
+      return 0
+   return round(num, significant_digits - int(math.floor(math.log10(abs(num)))) - 1)
