538c550655c693c4cd891c106aa353a7c06ec025
Ahmed-Ehab-601 <medo.losy@gmail.com>
2024-12-01 22:58:27 +0200

    gui Done

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from PyQt6 import QtWidgets, uic,QtCore
 from PyQt6.QtGui import QIntValidator, QBrush, QColor
 import numpy as np
 
-from v5.solver import Solver
+from Solver import Solver
 
 subscripts = {0: '₀', 1: '₁', 2: '₂', 3: '₃', 4: '₄', 5: '₅', 6: '₆', 7: '₇', 8: '₈', 9: '₉'}
 
