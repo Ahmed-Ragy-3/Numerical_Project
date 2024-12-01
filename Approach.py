717a557ebae7efc02c4500b65ede45f3b7c0ae12
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-12-01 18:16:29 +0200

    clear final branch

--- Approach.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-# Abstract base classes
-class Approach:
-   @abstractmethod
-   def solve(self):
-      """
-      Abstract method that must be implemented by subclasses
-      """
-      pass
