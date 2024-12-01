a088dd58f9fb1d93831f8a58ede7b0a0e386f0c8
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-12-01 22:48:44 +0200

    clear AhmedEhab branch

--- Direct_Methods/LU_Crout.py
+++
@@ -1,8 +0,0 @@
-from ..Iterative_Methods.Approach import Approach
-
-class Crout(Approach):
-   def __init__(self):
-      pass
-
-   def solve(self):
-      pass
--- Iterative_Methods/Jacobi.py
+++
@@ -1,8 +0,0 @@
-from .Approach import Approach
-
-class Jacobi(Approach):
-   def __init__(self):
-      pass
-   
-   def solve(self):
-      pass
